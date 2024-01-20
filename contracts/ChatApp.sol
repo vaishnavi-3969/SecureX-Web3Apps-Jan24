2eb99dbd2a03f6dd3790b7f5a99b29c11211bc9d
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2024-01-20 22:30:44 +0530

    Update app metadata and remove Lock contract

---
+++ contracts/ChatApp.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifider: MIT
+pragma solidity 0.8.19;
+
+contract ChatApp {
+    struct user {
+        string name;
+        friend[] friendList;
+    }
+    struct friend {
+        address pubkey;
+        string name;
+    }
+    struct message {
+        address sender;
+        uint256 timestamp;
+        string msg;
+    }
+    struct AllUserStruck{
+        string name;
+        address accountAddress;
+    }
+
+    AllUserStruck[] getAllUsers;
+
+    mapping(address => user) userList;
+    mapping(bytes32 => message[]) allMessages;
+
+    function checkUserExists(address pubkey) public view returns (bool) {
+        return bytes(userList[pubkey].name).length > 0;
+    }
+
+    function createAccount(string calldata name) external {
+        require(checkUserExists(msg.sender) == false, "User already exists");
+        require(bytes(name).length > 0, "Username can't be empty");
+        userList[msg.sender].name = name;
+        getAllUsers.push(AllUserStruck(name, msg.sender));
+    }
+
+    function getUsername(address pubkey) external view returns (string memory) {
+        require(checkUserExists(pubkey) == true, "User doesn't exist");
+        return userList[pubkey].name;
+    }
+
+    function addFriend(address friend_key, string calldata name) external {
+        require(checkUserExists(msg.sender) == true, "User doesn't exist");
+        require(
+            checkUserExists(friend_key),
+            "User doesn't exist/Not registered"
+        );
+        require(
+            msg.sender != friend_key,
+            "Users can't add themselves as friends"
+        );
+        require(
+            checkAlreadyFriends(msg.sender, friend_key) == false,
+            "these users are already friends"
+        );
+
+        _addFriend(msg.sender, friend_key, name);
+        _addFriend(friend_key, msg.sender, userList[msg.sender].name);
+    }
+
+    function checkAlreadyFriends(
+        address pubkey1,
+        address pubkey2
+    ) internal view returns (bool) {
+        if (
+            userList[pubkey1].friendList.length >
+            userList[pubkey2].friendList.length
+        ) {
+            address tmp = pubkey1;
+            pubkey1 = pubkey2;
+            pubkey2 = tmp;
+        }
+        for (uint256 i = 0; i < userList[pubkey1].friendList.length; i++) {
+            if (userList[pubkey1].friendList[i].pubkey == pubkey2) {
+                return true;
+            }
+        }
+        return false;
+    }
+    function _addFriend(address me, address friend_key, string memory name) internal {
+        friend memory newFriend = friend(friend_key, name);
+        userList[me].friendList.push(newFriend);
+    }
+    function getMyFriendList() external view returns (friend[] memory){
+        return userList[msg.sender].friendList;
+    }
+    function _getChatCode(address pubkey1, address pubkey2) internal pure returns (bytes32){
+        if(pubkey1 < pubkey2){
+            return keccak256(abi.encodePacked(pubkey1, pubkey2));
+        }else{
+            return keccak256(abi.encodePacked(pubkey2, pubkey1));
+        }
+    }
+    //send msg
+    function sendMessage(address friend_key, string calldata _msg) external {
+        require(checkUserExists(msg.sender), "Create an account first");
+        require(checkUserExists(friend_key), "User not registered");
+        require(
+            checkAlreadyFriends(msg.sender, friend_key),
+            "You are not friends with this user"
+        );
+        bytes32 chatCode = _getChatCode(msg.sender, friend_key);
+        message memory newMessage = message(msg.sender, block.timestamp, _msg);
+        allMessages[chatCode].push(newMessage);
+    }
+    function readMessage(address friend_key) external view returns(message[] memory) {
+        bytes32 chatCode = _getChatCode(msg.sender, friend_key);
+        return allMessages[chatCode];
+    }
+    function getAllAppUser() public view returns (AllUserStruck[] memory){
+        return getAllUsers;
+    }
+}
+
+
+
+
+
+
+
+
+
